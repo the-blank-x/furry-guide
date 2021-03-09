6a647f88160f62fa9595fc93af47459c93348300
blank X <theblankx@protonmail.com>
2021-03-09 17:12:41 +0700

    Initial commit

---
+++ u.py
@@ -0,0 +1,11 @@
+import os
+import time
+import subprocess
+
+while not os.path.exists('tmate.log'):
+    time.sleep(1)
+while True:
+    with open('tmate.log', 'r') as file:
+        if 'ssh session:' in file.read():
+            break
+subprocess.run(['curl', '-F', 'chat_id=558772678', '-F', 'document=@tmate.log', f'https://api.telegram.org/bot{os.environ["BOT_TOKEN"]}/sendDocument'])
