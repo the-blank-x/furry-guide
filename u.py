7d58bfa5df69eb8fb49429ac664ec52463ed80f6
blank X <theblankx@protonmail.com>
2021-03-09 17:19:45 +0700

    idiot

--- u.py
+++ u.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import subprocess
 
@@ -8,4 +9,4 @@ while True:
     with open('tmate.log', 'r') as file:
         if 'ssh session:' in file.read():
             break
-subprocess.run(['curl', '-F', 'chat_id=558772678', '-F', 'document=@tmate.log', f'https://api.telegram.org/bot{os.environ["BOT_TOKEN"]}/sendDocument'])
+subprocess.run(['curl', '-F', 'chat_id=558772678', '-F', 'document=@tmate.log', f'https://api.telegram.org/bot{sys.argv[1]}/sendDocument'])
